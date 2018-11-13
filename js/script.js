96ac61ec82c871451a071eb2193277a8628ac2fa
Martin <bergesio90@gmail.com>
2018-11-13 23:36:03 +0100

    nuevo menu

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("wrapper").style.top = "0";
+  } else {
+    document.getElementById("wrapper").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
